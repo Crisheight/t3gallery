6dc44927dae4a1d47a2e37d1c40ab9ae51ddb34b
Cris <crisheight@gmail.com>
2024-08-24 18:20:19 -0500

    Move topnav to be its own component

---
+++ src/app/_components/topnav.tsx
@@ -0,0 +1,20 @@
+import {SignInButton, SignedOut, UserButton, SignedIn} from "@clerk/nextjs";
+
+export function TopNav() {
+    return (
+        <nav className="flex w-full justify-between items-center p-4 text-xl
+        font-semibold">
+            <div>Gallery</div>
+
+            <div>
+                <SignedOut>
+                    <SignInButton />
+                </SignedOut>
+
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
+            </div>
+        </nav>
+    );
+}
