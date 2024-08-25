40d74d0a9db00e9298da8b5bb9ad6c991ed47153
Cris <crisheight@gmail.com>
2024-08-25 13:08:28 -0500

    begin separating images as component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,47 @@
-import Link from "next/link";
 import {db} from "~/server/db";
+import {SignedIn, SignedOut} from "@clerk/nextjs";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
-
+async function Images() {
     const images = await db.query.images.findMany({
-        orderBy: (model, { desc }) => desc(model.id),
+        orderBy: (model, {desc}) => desc(model.id),
     });
 
     return (
-        <main className="">
-            <div className="flex flex-wrap gap-4 justify-center">
+        <div className="flex flex-wrap gap-4 justify-center">
+            {images.map((image) => (
+                <div key={image.id} className="flex flex-col max-w-xs">
+                    <img
+                        src={image.url}
+                        alt="A list of streetphotography images in a row"
+                    />
+                    <div>{image.name}</div>
+                </div>
+            ))}
+        </div>
+    )
+}
 
-                { images.map((image) => (
-                    <div key={image.id} className="flex flex-col max-w-sm">
-                        <img src={image.url} alt="A list of streetphotography images in a row"/>
-                        <div>{image.name}</div>
-                    </div>
-                ))}
-            </div>
+export default async function HomePage() {
+
+    return (
+        <main className="">
+
+            <SignedOut>
+                <div className="w-full h-full text-2xl text-center mb-3">
+                    Sign in to view personalized gallery
+                </div>
+
+                <div>
+                    <Images />
+                </div>
+            </SignedOut>
+
+            <SignedIn>
+                <Images />
+            </SignedIn>
+
         </main>
     );
 }
