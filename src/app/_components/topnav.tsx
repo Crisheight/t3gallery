0198c0a0d1e1bf5a018654de8d8f4125a9218c47
Cris <crisheight@gmail.com>
2024-08-25 13:08:14 -0500

    add bottom border

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -3,7 +3,7 @@ import {SignInButton, SignedOut, UserButton, SignedIn} from "@clerk/nextjs";
 export function TopNav() {
     return (
         <nav className="flex w-full justify-between items-center p-4 text-xl
-        font-semibold">
+        font-semibold border-b-2">
             <div>Gallery</div>
 
             <div>
