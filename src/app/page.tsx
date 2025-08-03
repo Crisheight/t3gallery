4202c5285a1b1e91fef22a901defad5b14dcdbd7
Cris <crisheight@gmail.com>
2025-08-03 16:59:36 -0400

    Implement parallel route via @modal so that when a user clicks on a photo it returns the photo id -> url is changed but user remains on current page -> able to find the photo in feed after a refresh.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,66 +1,69 @@
-import { SignedIn, SignedOut } from "@clerk/nextjs";
-import { GetUserImages } from "~/server/queries";
-import { GetDefaultImages } from "~/server/queries";
+import {SignedIn, SignedOut} from "@clerk/nextjs";
+import {GetUserImages} from "~/server/queries";
+import {GetDefaultImages} from "~/server/queries";
 import Image from "next/image";
+import Link from "next/link";
 
 export const dynamic = "force-dynamic";
 
 async function DefaultImages() {
-  const images = await GetDefaultImages();
+    const images = await GetDefaultImages();
 
-  return (
-    <div className="flex flex-wrap justify-center gap-4">
-      {images.map((image) => (
-        <div key={image.id} className="flex max-w-xs flex-col">
-          <Image
-            src={image.url}
-            style={{ objectFit: "contain" }}
-            width={240}
-            height={240}
-            alt={image.name}
-          />
-          <div>{image.name}</div>
+    return (
+        <div className="flex flex-wrap justify-center gap-4">
+            {images.map((image) => (
+                <div key={image.id} className="flex max-w-xs flex-col">
+                    <Link href={`/photos/${image.id}`}>
+                    <Image
+                        src={image.url}
+                        style={{objectFit: "contain"}}
+                        width={240}
+                        height={240}
+                        alt={image.name}
+                    />
+                    </Link>
+                    <div>{image.name}</div>
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 }
 
 async function UserImages() {
-  const images = await GetUserImages();
+    const images = await GetUserImages();
 
-  return (
-    <div className="flex flex-wrap justify-center gap-4">
-      {images.map((image) => (
-        <div key={image.id} className="flex max-w-xs flex-col">
-          <Image
-            src={image.url}
-            style={{ objectFit: "contain" }}
-            width={400}
-            height={300}
-            alt={image.name}
-          />
-          <div>{image.name}</div>
+    return (
+        <div className="flex flex-wrap justify-center gap-4">
+            {images.map((image) => (
+                <div key={image.id} className="flex max-w-xs flex-col">
+                    <Image
+                        src={image.url}
+                        style={{objectFit: "contain"}}
+                        width={400}
+                        height={300}
+                        alt={image.name}
+                    />
+                    <div>{image.name}</div>
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 }
 
 export default async function HomePage() {
-  return (
-    <main className="">
-      <SignedOut>
-        <div className="mb-3 h-full w-full text-center text-2xl">
-          Sign in to upload photos
-        </div>
+    return (
+        <main className="">
+            <SignedOut>
+                <div className="mb-3 h-full w-full text-center text-2xl">
+                    Sign in to upload photos
+                </div>
 
-        <DefaultImages />
-      </SignedOut>
+                <DefaultImages/>
+            </SignedOut>
 
-      <SignedIn>
-        <UserImages />
-      </SignedIn>
-    </main>
-  );
+            <SignedIn>
+                <UserImages/>
+            </SignedIn>
+        </main>
+    );
 }
