275d2cdec63a4bd60f0fb721f6cb081c0e997c05
Cris <crisheight@gmail.com>
2025-08-03 18:11:48 -0400

    add getImageByID query -> Adjust naming convention for queries.

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -2,13 +2,13 @@ import "server-only";
 import {db} from "~/server/db";
 import {auth} from "@clerk/nextjs/server";
 
-export async function GetDefaultImages() {
+export async function getDefaultImages() {
     return db.query.images.findMany({
         orderBy: (model, {desc}) => desc(model.id),
     });
 }
 
-export async function GetUserImages() {
+export async function getUserImages() {
     const user = auth();
 
     if(!user.userId) throw new Error("Unauthorized");
@@ -17,4 +17,18 @@ export async function GetUserImages() {
         where: (model, {eq}) => eq(model.userId, user.userId),
         orderBy: (model, {desc}) => desc(model.id),
     });
+}
+
+export async function getImageById(id: number) {
+    const image = await db.query.images.findFirst({
+        where: (model, { eq }) => eq(model.id, id),
+    });
+
+    if (!image) throw new Error("Image not found");
+    if (!image.userId) return image;
+
+    const user = auth();
+    if (!user.userId || image.userId !== user.userId) throw new Error("Unauthorized");
+
+    return image;
 }
