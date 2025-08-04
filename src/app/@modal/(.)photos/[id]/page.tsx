190df259abf52a26bb6bc6eba534af2d5844ca80
Cris <crisheight@gmail.com>
2025-08-04 12:45:43 -0400

    updated clerk

--- src/app/@modal/(.)photos/[id]/page.tsx
+++ src/app/@modal/(.)photos/[id]/page.tsx
@@ -4,9 +4,9 @@ import {getImageById} from "~/server/queries";
 export default async function PhotoModal({
                                              params,
                                          }: {
-    params: Promise<{ id: string }>;
+    params: { id: string };
 }) {
-    const photoId = (await params).id;
+    const photoId = params.id;
 
     const idAsNumber = Number(photoId);
     if (isNaN(idAsNumber)) {
@@ -15,7 +15,9 @@ export default async function PhotoModal({
 
     const idImage = await getImageById(idAsNumber);
 
-    return <Modal>
-        <img src={idImage.url} className="w-96" alt={idImage.name} />
-    </Modal>;
+    return (
+        <Modal>
+            <img src={idImage.url} className="w-96" alt={idImage.name} />
+        </Modal>
+    );
 }
