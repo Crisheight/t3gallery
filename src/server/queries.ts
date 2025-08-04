6b1451c351de604402e1edf6d97b9a4445a50c45
Cris <crisheight@gmail.com>
2025-08-04 13:45:39 -0400

    Change query to use new DB item isPublic for images to determine user access (combined with auth). This lets anyone access a handful of images by default.

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -4,6 +4,7 @@ import {auth} from "@clerk/nextjs/server";
 
 export async function getDefaultImages() {
     return db.query.images.findMany({
+        where: (model, { eq }) => eq(model.isPublic, true),
         orderBy: (model, {desc}) => desc(model.id),
     });
 }
@@ -20,15 +21,22 @@ export async function getUserImages() {
 }
 
 export async function getImageById(id: number) {
+    const user = auth();
+
     const image = await db.query.images.findFirst({
         where: (model, { eq }) => eq(model.id, id),
     });
 
     if (!image) throw new Error("Image not found");
-    if (!image.userId) return image;
 
-    const user = auth();
-    if (!user.userId || image.userId !== user.userId) throw new Error("Unauthorized");
+    if (image.isPublic) {
+        return image;
+    }
+
+
+    if (!user.userId || image.userId !== user.userId) {
+        throw new Error("Unauthorized");
+    }
 
     return image;
 }
