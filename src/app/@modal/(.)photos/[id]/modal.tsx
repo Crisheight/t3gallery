43960b102ee999ac37750f9732acba2fdac786a8
Cris <crisheight@gmail.com>
2025-08-08 16:38:25 -0400

    Make dialog the container over div. Style the backdrop to half opacity in grey when a user clicks on an image.

--- src/app/@modal/(.)photos/[id]/modal.tsx
+++ src/app/@modal/(.)photos/[id]/modal.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { type ElementRef, useEffect, useRef } from 'react';
+import React, { type ElementRef, useEffect, useRef } from 'react';
 import { useRouter } from 'next/navigation';
 import { createPortal } from 'react-dom';
 
@@ -19,12 +19,10 @@ export function Modal({ children }: { children: React.ReactNode }) {
     }
 
     return createPortal(
-        <div className="modal-backdrop">
-            <dialog ref={dialogRef} className="modal" onClose={onDismiss}>
-                {children}
-                <button onClick={onDismiss} className="close-button" />
-            </dialog>
-        </div>,
-        document.getElementById('modal-root')!
+        <dialog ref={dialogRef} className="backdrop:bg-zinc-900/50" onClose={onDismiss}>
+            {children}
+            <button onClick={onDismiss} className="close-button" />
+        </dialog>,
+        document.getElementById('modal-root')!,
     );
 }
