ae24f4bae4a21162c126b7b81351943648c3beb5
Cris <crisheight@gmail.com>
2025-08-12 15:18:44 -0400

    Init: Wrap Image in divs, display Image and name. Reloads into full page via parallel route.

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -8,10 +8,22 @@ export default async function FullPageImage(props: { photoId: number }) {
 
     const idImage = await getImageById(props.photoId);
 
-    return <Image
-                src={idImage.url}
-                alt={idImage.name}
-                width={96}
-                height={69}
-    />;
+    return (
+        <div className="flex w-full h-full min-w-0 bg-green-500">
+            <div className="flex-shrink">
+                <Image
+                    src={idImage.url}
+                    alt={idImage.name}
+                    priority={true}
+                    width={1000}
+                    height={1000}
+                    style={{ width: "65%" , height: "auto"}}
+                    className="flex-shrink"
+                />
+            </div>
+            <div className="flex flex-col flex-shrink-0w-48">
+                <div className="text-xl font-bold">{idImage.name}</div>
+            </div>
+        </div>
+    );
 }
