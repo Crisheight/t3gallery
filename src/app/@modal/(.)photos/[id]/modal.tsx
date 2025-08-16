a8084dad71e001593716ccc6ff93b5c408af6924
Cris <crisheight@gmail.com>
2025-08-15 23:14:16 -0400

    add padding to selected images; lower images below navbar on homepage

--- src/app/@modal/(.)photos/[id]/modal.tsx
+++ src/app/@modal/(.)photos/[id]/modal.tsx
@@ -29,10 +29,10 @@ export function Modal({ children }: { children: React.ReactNode }) {
                 bg-transparent
                 text-white
                 backdrop:bg-zinc-900/50
+                pt-16
             "
         >
             {children}
-            {/*<button onClick={onDismiss} className="close-button"/>*/}
         </dialog>,
         document.getElementById('modal-root')!,
     );
