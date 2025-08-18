3f467d907ae8d5cc9ba3b37a8407b77c254979b2
Cris <crisheight@gmail.com>
2025-08-17 20:44:37 -0400

    idImage refactored to image (as the image is being used to pull more than the id). try check for the uploadername and display it.

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,27 +1,49 @@
 import {getImageById} from "~/server/queries";
 import Image from "next/image";
+import {clerkClient} from "@clerk/nextjs/server";
 
 export default async function FullPageImage(props: { photoId: number }) {
     if (isNaN(props.photoId)) {
         throw new Error("Invalid photo ID");
     }
 
-    const idImage = await getImageById(props.photoId);
+    const image = await getImageById(props.photoId);
+
+    const userId = image.userId;
+    let uploaderName = "Unknown user";
+
+    if (userId.startsWith("user_")) {
+        try {
+            const user = await clerkClient.users.getUser(userId);
+            uploaderName =
+                user.fullName ??
+                user.username ??
+                user.primaryEmailAddress?.emailAddress ??
+                "Unknown user";
+        } catch {
+            uploaderName = "Unknown user";
+        }
+    }
 
     return (
         <div className="flex h-[80vh] w-[90vw] max-w-[1200px] overflow-hidden rounded-lg bg-black/80">
             <div className="relative h-full flex-1 min-w-0">
                 <Image
-                    src={idImage.url}
-                    alt={idImage.name}
+                    src={image.url}
+                    alt={image.name}
                     priority
                     fill
                     className="object-cover"
                     sizes="(max-width: 1024px) 90vw, 70vw"
                 />
             </div>
-            <div className="w-72 min-w-0 border-l border-white/10 overflow-auto">
-                <div className="text-xl font-bold">{idImage.name}</div>
+            <div className="w-72 min-w-0 border-l border-white overflow-auto">
+                <div className="text-lg text-center border-b p-2">{image.name}</div>
+
+                <div className="flex flex-col p-2">
+                    <span>Uploaded By:</span>
+                    <span>{uploaderName}</span>
+                </div>
             </div>
         </div>
     );
