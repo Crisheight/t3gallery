bafea4cebc39cd5cd26ad2d3ff45240e9090b13a
Cris <crisheight@gmail.com>
2025-08-19 20:09:15 -0400

    Implemented new version recommendations

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,18 +1,26 @@
-import {auth} from "@clerk/nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
-import {db} from "~/server/db";
-import {images} from "~/server/db/schema";
-
+import { auth } from "@clerk/nextjs/server";
+import { db } from "~/server/db";
+import { images } from "~/server/db/schema";
 
 const f = createUploadthing();
 
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
     // Define as many FileRoutes as you like, each with a unique routeSlug
-    imageUploader: f({ image: { maxFileSize: "4MB", maxFileCount: 10 } })
+    imageUploader: f({
+        image: {
+            /**
+             * For full list of options and defaults, see the File Route API reference
+             * @see https://docs.uploadthing.com/file-routes#route-config
+             */
+            maxFileSize: "4MB",
+            maxFileCount: 10,
+        },
+    })
         // Set permissions and file types for this FileRoute
-        .middleware(async ({ req }) => {
+        .middleware(async () => {
             // This code runs on your server before upload
             const user = auth();
 
@@ -30,7 +38,7 @@ export const ourFileRouter = {
                 name: file.name,
                 url: file.url,
                 userId: metadata.userId,
-            })
+            });
 
             // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
             return { uploadedBy: metadata.userId };
